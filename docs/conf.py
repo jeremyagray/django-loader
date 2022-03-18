a50ff38852f4e5a346e165344337aaf27bfbc96d
Jeremy A Gray <jeremy.a.gray@gmail.com>
2022-03-17 22:01:44 -0500

    release: prepare to tag 0.0.7
    Github-closes: closes jeremyagray/django-loader#1
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Jeremy A Gray"
 author = "Jeremy A Gray"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.6"
+release = "0.0.7"
 
 
 # -- General configuration ---------------------------------------------------
